949f78d374d85b68db358c351a87587b222df4fb
dhishupvt1-tech <dhishupvt1@gmail.com>
2026-02-10 23:37:54 +0530

    Update authGuard.js

--- authGuard.js
+++ authGuard.js
@@ -1,11 +1,17 @@
 // authGuard.js
+
+// Hide page instantly
+document.documentElement.style.display = "none";
+
 async function requireAuth(supabaseClient) {
   const { data, error } = await supabaseClient.auth.getSession();
 
   if (error || !data.session) {
-    window.location.href = "login.html";
+    window.location.replace("login.html");
     return null;
   }
 
+  // Show page ONLY if authenticated
+  document.documentElement.style.display = "block";
   return data.session.user;
 }
